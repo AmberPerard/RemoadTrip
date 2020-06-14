ddc033f71fd1955ddcdae081b211e2c4e8651867
Moongirl1207 <amber.perard@student.howest.be>
2020-06-14 16:20:08 +0200

    started stream view

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -3,6 +3,34 @@ import { Link } from "react-router-dom";
 import { ROUTES } from "../consts";
 import style from "./Stream.module.css";
 
+const $video = document.getElementById("video");
+const $peerVideo = document.getElementById("peerVideo");
+const servers = {
+  iceServers: [
+    {
+      urls: `stun:stun.l.google.com:19302`,
+    },
+  ],
+};
+
+let myStream;
+let socket;
+let peerConnection;
+
+const startvideo = async () => {
+  const constraints = {
+    audio: false,
+    video: true,
+  };
+  myStream = await navigator.mediaDevices.getUserMedia(constraints);
+  $video.srcObject = myStream;
+  initSocket();
+};
+
+const initSocket = () => {
+  console.log("TODO: start socket connection");
+};
+
 const Stream = () => {
   return (
     <>
