5711dab3a6514c7bd8ac9163bd40e14d497501f4
ellen sierens <ellen.sierens@student.howest.be>
2020-06-17 13:38:07 +0200

    coordinaat uit socket van auto

--- src/components/MapBoxMap.js
+++ src/components/MapBoxMap.js
@@ -3,6 +3,10 @@ import React from "react";
 import mapboxgl from "mapbox-gl";
 // import Marker from "./components/Marker.js"
 // import ReactMapGL, { Marker } from "react-map-gl";
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "https://evening-caverns-60077.herokuapp.com/";
+let set = false
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiZWxsZW5zaWVyZW5zIiwiYSI6ImNrYmoyc2NwYzBqdjIyeXM3d3h2bW0xNGcifQ.AiHZhuCKL51mfLLdAf9dyQ";
@@ -16,10 +20,27 @@ class MapBoxMap extends React.Component {
       lat: 34,
       zoom: 2,
       userLocation: {},
+      location: {},
     };
+    this.socket = socketIOClient(ENDPOINT);
   }
 
   componentDidMount() {
+
+    if (set === false) {
+        set = true;
+        this.socket.emit("getCoords", (data) => {
+          console.log(data);
+  
+          this.setState({location: {
+              lat: parseFloat(data.latitude),
+              lng: parseFloat(data.longitude),
+          }});
+          new mapboxgl.Marker(el).setLngLat([this.state.location.lng, this.state.location.lat]).addTo(map);
+          console.log(this.state.location.lat, this.state.location.lng)
+        });
+      }
+
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/mapbox/streets-v11",
@@ -40,15 +61,19 @@ class MapBoxMap extends React.Component {
 
     var el = document.createElement("div");
     el.className = "autoMarker";
-    
-    el.addEventListener('click', function() {
-      window.alert("alert");
-      });
 
-    new mapboxgl.Marker(el).setLngLat([30.5, 50.5]).addTo(map);
+    el.addEventListener("click", function () {
+      window.alert("alert");
+    });
+
+    // if(set === true){
+    //     new mapboxgl.Marker(el).setLngLat([this.state.location.lat, this.state.location.lng]).addTo(map);
+    // }
 
     const nav = new mapboxgl.NavigationControl();
     map.addControl(nav, "top-left");
+
+
   }
 
   render() {
