18f1dc4cdc838047f06793cb3b8f6bba5065c213
ellen sierens <ellen.sierens@student.howest.be>
2020-06-19 13:04:39 +0200

    changed url for qr controllers

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -48,11 +48,11 @@ const Controller = () => {
         <div className={style.controllerContainer}>
           <h2 className={style.title}>Connecting the devices</h2>
           <div className={style.qrcodes}>
-            <QRCode value="https://gentle-cove-91397.herokuapp.com/" />
+            <QRCode value="https://int4controllers.herokuapp.com/" />
             <p className={style.link}>
               or surf to:&#8192;
-              <a href="https://gentle-cove-91397.herokuapp.com/">
-                https://gentle-cove-91397.herokuapp.com/
+              <a href="https://int4controllers.herokuapp.com/">
+              https://int4controllers.herokuapp.com/
               </a>
             </p>
           </div>
