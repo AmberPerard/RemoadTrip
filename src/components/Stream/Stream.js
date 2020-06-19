17dea1cfeb22e392d1404aa7f3dd2ed46a60b50d
Moongirl1207 <amber.perard@student.howest.be>
2020-06-19 16:36:41 +0200

    makeshift clock

--- src/components/Stream/Stream.js
+++ src/components/Stream/Stream.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { ROUTES } from "../../consts";
 import style from "./Stream.module.css";
 import TopContainerStreamView from "../TopContainerStreamView/TopContainerStreamView";
@@ -7,12 +7,43 @@ import BottomContainerStreamView from "../BottomContainerStreamView/BottomContai
 const Stream = () => {
   const [today, setToday] = useState(new Date());
   let time = today.toLocaleTimeString("en-BE");
+  let hours = useRef(0);
+  let minutes = useRef(0);
+
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
   const getTime = () => {
     setToday(new Date());
   };
 
+  useEffect(() => {
+    setIsActive(true);
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+
+    // 2
+    hours.current = Math.floor(seconds / 60 / 60);
+    console.log(hours);
+    console.log(minutes);
+    // 37
+    if (hours <= 0) {
+      minutes.current = Math.floor(seconds / 60);
+    } else {
+      minutes.current = Math.floor(seconds / 60);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   const setTime = setInterval(getTime, 1 * 1000);
+  console.log(seconds);
 
   useEffect(() => {
     return () => {
@@ -42,7 +73,7 @@ const Stream = () => {
           scrolling="no"
         ></iframe>
         <BottomContainerStreamView
-          timeDriven={"00:00"}
+          timeDriven={`${minutes.current}:${seconds}`}
           location={"Brugge, Belgium"}
           timeLocal={time}
           route={ROUTES.finish}
