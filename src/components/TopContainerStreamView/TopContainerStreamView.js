c2febaf9d5ae4c44abfb20bc47adc0de1fa3ca19
Moongirl1207 <amber.perard@student.howest.be>
2020-06-22 14:02:20 +0200

    fixed top view

--- src/components/TopContainerStreamView/TopContainerStreamView.js
+++ src/components/TopContainerStreamView/TopContainerStreamView.js
@@ -4,30 +4,22 @@ import style from "./TopContainerStreamView.module.css";
 const TopContainerStreamView = ({ tokensFound, mtoToken, driving }) => {
   return (
     <>
-      <div className={`${style.topContainer} ${driving ? "" : style.grey}`}>
-        <p className={style.tokenFound}>{tokensFound}</p>
-        {driving ? (
-          <img
-            className={`${style.compassNoFilter} ${
-              driving ? "" : style.noFilter
-            }`}
-            src="./assets/Compass.png"
-            alt="compass for the car to find the tokens"
-            width="95px"
-            height="95px"
-          ></img>
-        ) : (
-          <img
-            className={`${style.compassNoFilter} ${
-              driving ? "" : style.noFilter
-            }`}
-            src="./assets/Compass.png"
-            alt="compass for the car to find the tokens"
-            width="95px"
-            height="95px"
-          ></img>
-        )}
-        <p className={style.tokenNear}>{mtoToken}m</p>
+      <div className={`${style.topContainer}`}>
+        <p className={`${driving ? style.tokenFound : style.tokenFoundGrey}`}>
+          {tokensFound}
+        </p>
+        <img
+          className={`${style.compassNoFilter} ${
+            driving ? "" : style.noFilter
+          }`}
+          src="./assets/Compass.png"
+          alt="compass for the car to find the tokens"
+          width="95px"
+          height="95px"
+        ></img>
+        <p className={`${driving ? style.tokenNear : style.tokenNearGrey}`}>
+          {mtoToken}m
+        </p>
       </div>
     </>
   );
